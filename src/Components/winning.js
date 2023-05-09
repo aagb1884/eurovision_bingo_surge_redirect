c6bc4ece89d6268b3c25ae2d04f2e0245a86f370
Andrew Blair <a.a.g.blair@gmail.com>
2023-05-09 22:08:43 +0100

    tidying up code

--- src/Components/winning.js
+++ src/Components/winning.js
@@ -1,16 +1,6 @@
-// import React, {useRef } from "react";
 import soundfile from "../audio/euphoria_excerpt.mp3";
 
 const Winning = () => {
-
-//    const audio = useRef(null)
-//    const playAudio = () => {
-//     console.log(audio.current);
-//     audio.current.play();
-//    }
-    // const Bingo = () => {
-    //     const [play] = useSound(audio);
-
 
     function play() {
                 var audio = document.getElementById("audio");
@@ -18,42 +8,19 @@ const Winning = () => {
 
               }
 
-// const audioElement = new Audio()
-// audioElement.src = audio
-// const playAudio = audioElement.play()
-// } 
-
 function disableButton() {
     document.getElementById("bingo").disabled = true;
 }
-
+// get grid buttons working then set button below to be disabled until all six buttons are pressed/active
 
     return ( 
         <div className='bingo'>
-            {/* <div id="display" onClick={playAudio}></div> */}
             <audio id="audio" src={soundfile}>BINGO</audio>
             <input type="button" id="bingo" value="BINGO" onClick={play}></input>
-            {/* <button type="button" id="bingo" class="bingo-btn" onClick={playAudio}>BINGO!</button> */}
              </div>
     );
     }
 
 
-
 export default Winning;
 
-
-    
- // const audio = new Audio("./audio/euphoria_excerpt.mp3");
-    // const audioButtons = document.querySelectorAll('.bingo');
-    
-    // audioButtons.forEach(button => {
-    //   button.addEventListener("click", () => {
-    //     audio.play();
-    //   });
-    // });
-
-    // function play() {
-    //     var audio = document.getElementById("audio");
-    //     audio.play();
-    //   }
